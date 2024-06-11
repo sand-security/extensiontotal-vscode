a2b5e9cfa610f95042594cca61b7bd3cfe1f03e5
Amit Assaraf <amit.assaraf@gmail.com>
2024-06-11 23:24:06 +0300

    chore: first commit

---
+++ src/extension.js
@@ -0,0 +1,14 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate,
+};
